91ad0c46f797f89d488d0ab25069183f14c4db24
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-09-02 11:32:30 +0100

    feat: add exit command

--- app/main.go
+++ app/main.go
@@ -12,14 +12,20 @@ var _ = fmt.Fprint
 func main() {
 	// Uncomment this block to pass the first stage
 
-	for true {
+	for {
 		fmt.Fprint(os.Stdout, "$ ")
+		
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprint(os.Stderr, "Error reading command: ", err)
 			os.Exit(1)
 			break
 		}
+		if command[:len(command)-1] == "exit 0" {
+			os.Exit(0)
+			break
+		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
+		
 	}
 }
