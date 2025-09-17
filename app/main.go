972b0afe9047011e4453ef67f95a4f8a389fa17c
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-09-17 09:16:29 +0100

    autocompletion progress

--- app/main.go
+++ app/main.go
@@ -78,16 +78,15 @@ func (c *ShellCompleter) Do(line []rune, pos int) ([][]rune, int) {
             return [][]rune{[]rune(completion)}, len(input)
         }
         fmt.Fprint(os.Stdout, "\n")
-        for i, m := range matches {
-            if i > 0 {
-                fmt.Fprint(os.Stdout, "  ")
-            }
-            fmt.Fprint(os.Stdout, m)
-        }
-        fmt.Fprintf(os.Stdout, "\n$ %s", input)
+     //    for i, m := range matches {
+     //        if i > 0 {
+     //            fmt.Fprint(os.Stdout, "  ")
+     //        }
+     //        fmt.Fprint(os.Stdout, m)
+     //    }
+     //    fmt.Fprintf(os.Stdout, "\n$ %s", input)
         return nil, 0
     }
-    // ...
     	return nil, 0
 }
 
